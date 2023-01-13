d4a40dbe737475ca0da52c4e84eb52afac072513
Balogun Razak <razak2free@gmail.com>
2023-01-13 23:47:33 +0100

    Upadte

--- components/TabIcons.js
+++ components/TabIcons.js
@@ -2,12 +2,25 @@ import { View, Text, Image } from 'react-native'
 import {FONTS, COLORS} from '../constants'
 import React from 'react'
 
-const TabIcons = () => {
-  return (
-    <View>
-      <Text>TabIcons</Text>
+const TabIcons = ({ focused, icon, iconStyle, lable, isTrade}) => {
+    if (isTrade) {
+        return (
+        <View>
+        <Text style={{color:COLORS.white}}>Trade</Text>
     </View>
-  )
+        )
+    } else {
+        return (
+        <View>
+        <Text style={{color:COLORS.white}}>Tab</Text>
+    </View>
+        )
+    }
+//   return (
+//     <View>
+//         <Text>Tab</Text>
+//     </View>
+//   )
 }
 
 export default TabIcons
--- navigation/tabs.js
+++ navigation/tabs.js
@@ -5,7 +5,7 @@ import {
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { TabIcons } from "../components";
 import { Home, Portfolio, Market, Profile } from "../screens"
-import { COLORS } from "../constants"
+import { COLORS, icons } from "../constants"
 
 const Tab = createBottomTabNavigator()
 
@@ -14,31 +14,91 @@ const Tabs = () => {
     return (
         <Tab.Navigator
             tabBarOptions={{
+                showLabel: false,
                 style: {
+                    height: 140,
                     backgroundColor: COLORS.primary,
-                    borderTopColor: "transparent",
+                    borderTopLeftRadius: 10,
+                    borderTopRightRadius: 10,
+                    
                 }
             }}
         >
             <Tab.Screen
                 name="Home"
                 component={Home}
+                options= {{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcons 
+                            focused={focused}
+                            icon={icons.home}
+                            label= "Home"
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Portfolio"
                 component={Portfolio}
+                options= {{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcons 
+                            focused={focused}
+                            icon={icons.briefcase}
+                            label= "Portfolio"
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Trade"
                 component={Home}
+                options= {{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcons 
+                            focused={focused}
+                            icon={icons.trade}
+                            label= "Trade"
+                            isTrade= {true}
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Market"
                 component={Market}
+                options= {{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcons 
+                            focused={focused}
+                            icon={icons.market}
+                            label= "Market"
+                            />
+                        )
+                    }
+                }}
             />
             <Tab.Screen
                 name="Profile"
                 component={Profile}
+                options= {{
+                    tabBarIcon: ({focused}) => {
+                        return (
+                            <TabIcons 
+                            focused={focused}
+                            icon={icons.profile}
+                            label= "Profile"
+                            />
+                        )
+                    }
+                }}
             />
         </Tab.Navigator>
     )
