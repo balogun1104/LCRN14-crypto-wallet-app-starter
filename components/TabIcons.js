5bde06baca69d89db304ab72782fd1bf3330c98b
Balogun Razak <razak2free@gmail.com>
2023-01-13 23:58:33 +0100

    Update TabIcons.js

--- components/TabIcons.js
+++ components/TabIcons.js
@@ -1,26 +1,37 @@
-import { View, Text, Image } from 'react-native'
-import {FONTS, COLORS} from '../constants'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import { FONTS, COLORS } from "../constants";
+import React from "react";
 
-const TabIcons = ({ focused, icon, iconStyle, lable, isTrade}) => {
-    if (isTrade) {
-        return (
-        <View>
-        <Text style={{color:COLORS.white}}>Trade</Text>
-    </View>
-        )
-    } else {
-        return (
-        <View>
-        <Text style={{color:COLORS.white}}>Tab</Text>
-    </View>
-        )
-    }
-//   return (
-//     <View>
-//         <Text>Tab</Text>
-//     </View>
-//   )
-}
+const TabIcons = ({ focused, icon, iconStyle, lable, isTrade }) => {
+  if (isTrade) {
+    return (
+      <View>
+        <Text style={{ color: COLORS.white }}>Trade</Text>
+      </View>
+    );
+  } else {
+    return <View
+    style={{
+        alignItems: 'center', justifyContent: 'center'
+    }}
+    >
+        <Image 
+        source={icon}
+        resizeMode= 'contain'
+        style={{
+            height: 25,
+            width: 25,
+            tintColor: focused ? COLORS.white : COLORS.secondary,
+            ...iconStyle
+        }}
+        />
+    </View>;
+  }
+  //   return (
+  //     <View>
+  //         <Text>Tab</Text>
+  //     </View>
+  //   )
+};
 
-export default TabIcons
+export default TabIcons;
